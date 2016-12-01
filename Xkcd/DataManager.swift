0848e5789450bf058568bab762cd589ca37b05f4
Eric Chang <echang25@hotmail.com>
2016-12-01 14:54:12 -0500

    initial commit with files

---
+++ Xkcd/DataManager.swift
@@ -0,0 +1,9 @@
+//
+//  DataManager.swift
+//  Xkcd
+//
+//  Created by Eric Chang on 12/1/16.
+//  Copyright © 2016 Eric Chang. All rights reserved.
+//
+
+import Foundation
---
+++ Xkcd/XkcdViewController.swift
@@ -0,0 +1,35 @@
+//
+//  XkcdViewController.swift
+//  Xkcd
+//
+//  Created by Eric Chang on 12/1/16.
+//  Copyright © 2016 Eric Chang. All rights reserved.
+//
+
+import UIKit
+
+class XkcdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
