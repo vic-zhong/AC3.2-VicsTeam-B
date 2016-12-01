95aeddee3b6bca6ab6cfbda153edeed73c41e4ec
Tong Lin <tonglin.t91@gmail.com>
2016-12-01 16:32:40 -0500

    presenting view

---
+++ Xkcd/PresentedViewController.swift
@@ -0,0 +1,188 @@
+//
+//  PresentedViewController.swift
+//  Xkcd
+//
+//  Created by Tong Lin on 12/1/16.
+//  Copyright © 2016 Eric Chang. All rights reserved.
+//
+
+import UIKit
+import View2ViewTransition
+
+class PresentedViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UIGestureRecognizerDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.edgesForExtendedLayout = []
+        
+        self.navigationItem.titleView = self.titleLabel
+        self.navigationItem.leftBarButtonItem = self.backItem
+        self.view.backgroundColor = UIColor.white
+        
+        self.view.addSubview(self.collectionView)
+        self.view.addSubview(self.closeButton)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if let gestureRecognizers = self.view.gestureRecognizers {
+            for gestureRecognizer in gestureRecognizers {
+                if let panGestureRecognizer = gestureRecognizer as? UIPanGestureRecognizer {
+                    panGestureRecognizer.delegate = self
+                }
+            }
+        }
+    }
+    
+    // MARK: Elements
+    
+    weak var transitionController: TransitionController!
+    
+    lazy var collectionView: UICollectionView = {
+        
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.itemSize = self.view.bounds.size
+        layout.minimumLineSpacing = 0
+        layout.minimumInteritemSpacing = 0
+        layout.scrollDirection = .horizontal
+        
+        let collectionView: UICollectionView = UICollectionView(frame: self.view.bounds, collectionViewLayout: layout)
+        collectionView.register(PresentedCollectionViewCell.self, forCellWithReuseIdentifier: "presented_cell")
+        collectionView.backgroundColor = UIColor.white
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.isPagingEnabled = true
+        return collectionView
+    }()
+    
+    lazy var closeButton: UIButton = {
+        let frame: CGRect = CGRect(x: 0.0, y: 20.0, width: 60.0, height: 40.0)
+        let button: UIButton = UIButton(frame: frame)
+        button.setTitle("Close", for: .normal)
+        button.setTitleColor(self.view.tintColor, for: .normal)
+        button.addTarget(self, action: #selector(onCloseButtonClicked(sender:)), for: .touchUpInside)
+        return button
+    }()
+    
+    lazy var titleLabel: UILabel = {
+        let font: UIFont = UIFont.boldSystemFont(ofSize: 16.0)
+        let label: UILabel = UILabel()
+        label.font = font
+        label.text = "Detail"
+        label.sizeToFit()
+        return label
+    }()
+    
+    lazy var backItem: UIBarButtonItem = {
+        let item: UIBarButtonItem = UIBarButtonItem(title: "< Back", style: .plain, target: self, action: #selector(onBackItemClicked(sender:)))
+        return item
+    }()
+    
+    // MARK: CollectionView Data Source
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell: PresentedCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "presented_cell", for: indexPath) as! PresentedCollectionViewCell
+        cell.contentView.backgroundColor = UIColor.white
+        
+        let number: Int = indexPath.item%4 + 1
+        cell.content.image = UIImage(named: "image\(number)")
+        
+        return cell
+    }
+    
+    // MARK: Actions
+    
+    func onCloseButtonClicked(sender: AnyObject) {
+        
+        let indexPath: NSIndexPath = self.collectionView.indexPathsForVisibleItems.first! as NSIndexPath
+        
+        self.transitionController.userInfo = ["destinationIndexPath": indexPath, "initialIndexPath": indexPath]
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func onBackItemClicked(sender: AnyObject) {
+        
+        let indexPath: NSIndexPath = self.collectionView.indexPathsForVisibleItems.first! as NSIndexPath
+        self.transitionController.userInfo = ["destinationIndexPath": indexPath, "initialIndexPath": indexPath]
+        
+        if let navigationController = self.navigationController {
+            navigationController.popViewController(animated: true)
+        }
+    }
+    
+    // MARK: Gesture Delegate
+    
+    func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        
+        let indexPath: NSIndexPath = self.collectionView.indexPathsForVisibleItems.first! as NSIndexPath
+        self.transitionController.userInfo = ["destinationIndexPath": indexPath, "initialIndexPath": indexPath]
+        
+        let panGestureRecognizer: UIPanGestureRecognizer = gestureRecognizer as! UIPanGestureRecognizer
+        let transate: CGPoint = panGestureRecognizer.translation(in: self.view)
+        return Double(abs(transate.y)/abs(transate.x)) > M_PI_4
+    }
+}
+
+extension PresentedViewController: View2ViewTransitionPresented {
+    
+    func destinationFrame(_ userInfo: [String: AnyObject]?, isPresenting: Bool) -> CGRect {
+        
+        let indexPath: IndexPath = userInfo!["destinationIndexPath"] as! IndexPath
+        let cell: PresentedCollectionViewCell = self.collectionView.cellForItem(at: indexPath) as! PresentedCollectionViewCell
+        return cell.content.frame
+    }
+    
+    func destinationView(_ userInfo: [String: AnyObject]?, isPresenting: Bool) -> UIView {
+        
+        let indexPath: IndexPath = userInfo!["destinationIndexPath"] as! IndexPath
+        let cell: PresentedCollectionViewCell = self.collectionView.cellForItem(at: indexPath) as! PresentedCollectionViewCell
+        return cell.content
+    }
+    
+    func prepareDestinationView(_ userInfo: [String: AnyObject]?, isPresenting: Bool) {
+        
+        if isPresenting {
+            
+            let indexPath: IndexPath = userInfo!["destinationIndexPath"] as! IndexPath
+            let contentOfffset: CGPoint = CGPoint(x: self.collectionView.frame.size.width*CGFloat(indexPath.item), y: 0.0)
+            self.collectionView.contentOffset = contentOfffset
+            
+            self.collectionView.reloadData()
+            self.collectionView.layoutIfNeeded()
+        }
+    }
+}
+
+public class PresentedCollectionViewCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.contentView.addSubview(self.content)
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public lazy var content: UIImageView = {
+        let margin: CGFloat = 2.0
+        let width: CGFloat = (UIScreen.main.bounds.size.width - margin*2.0)
+        let height: CGFloat = (UIScreen.main.bounds.size.height - 160.0)
+        let frame: CGRect = CGRect(x: margin, y: (UIScreen.main.bounds.size.height - height)/2.0, width: width, height: height)
+        let view: UIImageView = UIImageView(frame: frame)
+        view.backgroundColor = UIColor.gray
+        view.clipsToBounds = true
+        view.contentMode = .scaleAspectFill
+        return view
+    }()
+}
--- Xkcd/XkcdViewController.swift
+++ Xkcd/XkcdViewController.swift
@@ -7,25 +7,146 @@
 //
 
 import UIKit
+import View2ViewTransition
 
-class XkcdViewController: UIViewController {
+class XkcdViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     @IBOutlet weak var testLabel: UILabel!
 
     let apiClient = APIManager()
     
     var comic: Comic?
+    var ourImage: UIImage?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         somefunc()
+        self.view.addSubview(self.collectionView)
     }
 
+    func loadImage(url: URL) {
+        let shareSession = URLSession.shared
+        let downloadTask = shareSession.downloadTask(with: url, completionHandler: { (location: URL?, response: URLResponse?, error: Error?) -> Void in
+            if location != nil {
+                DispatchQueue.main.async {
+                    let data:Data! = try? Data(contentsOf: location!)
+                    let image = UIImage(data: data)
+                    self.ourImage = image
+                    self.collectionView.reloadData()
+                }
+            }
+        })
+        downloadTask.resume()
+    }
+
+    
+    let transitionController: TransitionController = TransitionController()
+    
+    var selectedIndexPath: IndexPath = IndexPath(item: 0, section: 0)
+    
+    lazy var collectionView: UICollectionView = {
+        
+        let lendth: CGFloat = (UIScreen.main.bounds.size.width - 4.0)/3.0
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: lendth, height: lendth)
+        layout.minimumLineSpacing = 1.0
+        layout.minimumInteritemSpacing = 1.0
+        layout.scrollDirection = .vertical
+        
+        let collectionView: UICollectionView = UICollectionView(frame: self.view.bounds, collectionViewLayout: layout)
+        collectionView.register(PresentingCollectionViewCell.self, forCellWithReuseIdentifier: "presenting_cell")
+        collectionView.backgroundColor = UIColor.white
+        collectionView.contentInset = UIEdgeInsets(top: 1.0, left: 1.0, bottom: 1.0, right: 1.0)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        return collectionView
+    }()
+    
+    lazy var titleLabel: UILabel = {
+        let font: UIFont = UIFont(name: "Futura-Medium", size: 16.0)!
+        let label: UILabel = UILabel()
+        label.font = font
+        label.text = "All"
+        label.sizeToFit()
+        return label
+    }()
+    
+    lazy var closeItem: UIBarButtonItem = {
+        let item: UIBarButtonItem = UIBarButtonItem(title: "Close", style: .plain, target: self, action: #selector(onCloseButtonClicked(sender:)))
+        return item
+    }()
+    
+    // MARK: CollectionView Delegate
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        self.selectedIndexPath = indexPath
+        
+        let presentedViewController: PresentedViewController = PresentedViewController()
+        
+        presentedViewController.transitionController = self.transitionController
+        transitionController.userInfo = ["destinationIndexPath": indexPath as NSIndexPath, "initialIndexPath": indexPath as NSIndexPath]
+        
+        // This example will push view controller if presenting view controller has navigation controller.
+        // Otherwise, present another view controller
+        if let navigationController = self.navigationController {
+            
+            // Set transitionController as a navigation controller delegate and push.
+            navigationController.delegate = transitionController
+            transitionController.push(viewController: presentedViewController, on: self, attached: presentedViewController)
+            
+        } else {
+            
+            // Set transitionController as a transition delegate and present.
+            presentedViewController.transitioningDelegate = transitionController
+            transitionController.present(viewController: presentedViewController, on: self, attached: presentedViewController, completion: nil)
+        }
+        
+        collectionView.deselectItem(at: indexPath, animated: true)
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        
+        if let _ = self.navigationController { return }
+        
+        if scrollView.contentOffset.y <= -100.0 {
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    // MARK: CollectionView Data Source
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell: PresentingCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "presenting_cell", for: indexPath) as! PresentingCollectionViewCell
+        cell.contentView.backgroundColor = UIColor.lightGray
+        let number: Int = indexPath.item%4 + 1
+        cell.content.image = ourImage
+        
+        return cell
+    }
+    
+        // MARK: Actions
+    
+    func onCloseButtonClicked(sender: AnyObject) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
     func somefunc() {
         apiClient.getData(completionHandler: { results in
             DispatchQueue.main.async {
                 if let results = results {
                     self.comic = results
                     self.testLabel.text = self.comic?.image
+                    self.loadImage(url: URL(string: (self.comic?.image)!)!)
+                    
                 }
                 
             }
@@ -43,4 +164,55 @@ class XkcdViewController: UIViewController {
     }
     */
 
+}
+
+
+extension XkcdViewController: View2ViewTransitionPresenting {
+    
+    func initialFrame(_ userInfo: [String: AnyObject]?, isPresenting: Bool) -> CGRect {
+        
+        guard let indexPath: IndexPath = userInfo?["initialIndexPath"] as? IndexPath, let attributes: UICollectionViewLayoutAttributes = self.collectionView.layoutAttributesForItem(at: indexPath) else {
+            return CGRect.zero
+        }
+        return self.collectionView.convert(attributes.frame, to: self.collectionView.superview)
+    }
+    
+    func initialView(_ userInfo: [String: AnyObject]?, isPresenting: Bool) -> UIView {
+        
+        let indexPath: IndexPath = userInfo!["initialIndexPath"] as! IndexPath
+        let cell: UICollectionViewCell = self.collectionView.cellForItem(at: indexPath)!
+        
+        return cell.contentView
+    }
+    
+    func prepareInitialView(_ userInfo: [String : AnyObject]?, isPresenting: Bool) {
+        let indexPath: IndexPath = userInfo!["initialIndexPath"] as! IndexPath
+        
+        if !isPresenting && !self.collectionView.indexPathsForVisibleItems.contains(indexPath) {
+            self.collectionView.reloadData()
+            self.collectionView.scrollToItem(at: indexPath, at: .centeredVertically, animated: false)
+            self.collectionView.layoutIfNeeded()
+        }
+    }
+}
+
+public class PresentingCollectionViewCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.contentView.addSubview(self.content)
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public lazy var content: UIImageView = {
+        let view: UIImageView = UIImageView(frame: self.contentView.bounds)
+        view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.backgroundColor = UIColor.gray
+        view.clipsToBounds = true
+        view.contentMode = .scaleAspectFill
+        return view
+    }()
 }
